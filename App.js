f37174dd266a962b0d73d6292903b13754b0c1b5
bveinz <velosobruno61@gmail.com>
2024-07-19 14:58:14 -0400

    mi first bottom taps app

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native';
+import WelcomeScreen from './screens/WelcomeScreen'
+import UserScreen from './screens/UserScreen'
+import { Ionicons } from '@expo/vector-icons'
+
+const BottomTap = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style='light'/>
+    <NavigationContainer>
+      <BottomTap.Navigator screenOptions={{
+        headerStyle: { backgroundColor: '#3c0a6b'},
+        headerTintColor: 'white',
+        tabBarActiveTintColor: '#3c0a6b'
+      }}>
+        <BottomTap.Screen name="Welcome" component={WelcomeScreen} 
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Ionicons name="home" color={color} size={size} />
+            )
+          }}
+        />
+        <BottomTap.Screen name="User" component={UserScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Ionicons name="person" color={color} size={size} />
+            )
+          }}
+        />
+      </BottomTap.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
